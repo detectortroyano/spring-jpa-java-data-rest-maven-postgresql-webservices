62546f5bbfc63becdd4121d4bf65e1311beec504
Angel Ricardo Vega Hernandez <angelricardo.uthh@gmail.com>
2015-09-19 16:30:21 -0500

    Ejemplos de webservices extras

--- SpringDataRestExample/src/main/java/com/jcg/examples/repo/UserRepo.java
+++ SpringDataRestExample/src/main/java/com/jcg/examples/repo/UserRepo.java
@@ -3,6 +3,7 @@ package com.jcg.examples.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -12,6 +13,12 @@ import com.jcg.examples.entity.User;
 @RepositoryRestResource
 public interface UserRepo extends JpaRepository<User, Long>
 {
+		//http://localhost:8080/SpringDataRestExample/api/users/search/byName?name=angel
 		@RestResource(path="byName") 
 		public List<User> findByName(@Param("name") String name);
+		
+		//http://localhost:8080/SpringDataRestExample/api/users/search/findByEmail?email=angelricardo.uthh@gmail.com
+	    @Query("SELECT u FROM User u "
+	            +" WHERE u.email = :email")
+	    List<User> findByEmail(@Param("email")String email);		
 }
